26862a0a254d2d1ddde816cb05b2c174b06879b5
Raisess <raisechif@gmail.com>
2021-04-05 12:24:52 -0300

    added a display prop to message bubble component, can hide/show the component any time

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -3,11 +3,12 @@ import "./styles/message_bubble.css";
 interface IMessageBubbleProps {
 	profilePicture: string;
 	message:        string;
+	display:        boolean;
 }
 
-export default function MessageBubble({ profilePicture, message }: IMessageBubbleProps): JSX.Element {
+export default function MessageBubble({ profilePicture, message, display }: IMessageBubbleProps): JSX.Element {
 	return (
-		<div className="message-bubble">
+		<div className="message-bubble" style={{ display: display ? "" : "none" }}>
 			<div className="message-bubble-flex-container">
 				<img alt="profile_picture" src={ profilePicture } />
 				<div className="message-bubble-msg-container">
