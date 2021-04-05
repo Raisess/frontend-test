4cc496fc69d2f435beac8603c797b27feb109dad
Raisess <raisechif@gmail.com>
2021-04-05 15:06:03 -0300

    user schema yup validation

--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -1,9 +1,21 @@
 import "./styles/save_button.css";
 
-export default function SaveButton(): JSX.Element {
+import userSchema from "../schemas/userSchema";
+
+export default function SaveButton({ userProps }: any): JSX.Element {
 	return (
 		<div className="save-button-container">
-			<button className="save-button">Salvar</button>
+			<button
+				className="save-button"
+				onClick={async (): Promise<void> => {
+					console.log(userProps);
+					const isValid: boolean = await userSchema.isValid(userProps);
+
+					console.log(isValid);
+				}}
+			>
+				Salvar
+			</button>
 		</div>
 	);
 }
---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,12 @@
+import * as yup from "yup";
+
+const userSchema: yup.ObjectSchema<any> = yup.object().shape({
+	username: yup.string().required(),
+	address:  yup.string().required(),
+	birthday: yup.date().required(),
+	email:    yup.string().email().required(),
+	stars:    yup.number().required()
+});
+
+export default userSchema;
+
